f196cd85dd17be5d7fb285eac6ab7b3451610436
kellyma <martin.kelly@ros.gov.uk>
2017-11-14 14:23:21 +0000

    minor updates

---
+++ tasks.py
@@ -0,0 +1,13 @@
+from celery import Celery
+
+app = Celery('tasks', backend='amqp', broker='amqp://guest@localhost//')
+app.config_from_object('celeryconfig')
+
+
+@app.task
+def hello_world():
+    return 'hello world'
+
+
+if __name__ == '__main__':
+    app.start()
